7db8dfb9211fafe66c01988c5a699e4ab78151e3
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-07-11 15:50:04 +0300

    Updated

---
+++ ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExplicitInterfaces
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] personInfo = input
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                IPerson person = new Citizen(personInfo[0], personInfo[1], int.Parse(personInfo[2]));
+                Console.WriteLine(person.GetName());
+
+                IResident resident = new Citizen(personInfo[0], personInfo[1], int.Parse(personInfo[2]));
+                Console.WriteLine(resident.GetName());
+            }
+        }
+    }
+}
